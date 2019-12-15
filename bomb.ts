f2de377f96cc0dc91432c01b169a96e4fba0b48c
Peli de Halleux <pelikhan@users.noreply.github.com>
2019-12-14 21:36:57 -0800

    Update pxt.json, bomb.ts

--- bomb.ts
+++ bomb.ts
@@ -1,44 +1,11 @@
 basic.showString("BOMB")
 
-let state = 0;
-
-function reset() {
-    state = 0;    
-}
-reset();
-
-function win() {
-    if (!state)
-        state = escape.BOMB_DEACTIVATED;    
-}
-input.onPinPressed(TouchPin.P0, win)
-
-radio.onReceivedBuffer(b => {
-    escape.logMessage(b);
-    switch(b[0]) {
-        case escape.RESET:
-            reset();
-            break;
-        case escape.TIME_OVER:
-            if (!state)
-                state = escape.TIME_OVER;
-            break;
-    }
+// send deactivation signal on button pressed
+input.onPinPressed(TouchPin.P0, function() {
+    for(let i = 0; i < 5; ++i)
+        escape.broadcastMessage(escape.BOMB_DEACTIVATED);
 })
 
-basic.forever(function() {
-    switch(state) {
-        case escape.TIME_OVER:
-            escape.showLose();
-            break;
-        case escape.BOMB_DEACTIVATED:
-            const b = control.createBuffer(1)
-            b[0] = escape.BOMB_DEACTIVATED;
-            radio.sendBuffer(b);
-            escape.showWin();
-            break;
-        default:
-            led.toggle(Math.randomRange(0, 4), Math.randomRange(0, 4))
-            break;
-    }
+escape.onUpdate(function () {
+    led.toggle(Math.randomRange(0, 4), Math.randomRange(0, 4))
 })
