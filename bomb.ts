d9b93b6c55612e2e0a5c2af11b38afa9adb0ffdc
Peli de Halleux <pelikhan@users.noreply.github.com>
2020-01-04 17:15:10 -0800

    Update bomb.ts

--- bomb.ts
+++ bomb.ts
@@ -2,8 +2,7 @@ basic.showString("BOMB")
 
 // send deactivation signal on button pressed
 input.onPinPressed(TouchPin.P0, function() {
-    for(let i = 0; i < 5; ++i)
-        escape.broadcastMessage(escape.BOMB_DEACTIVATED);
+    escape.broadcastMessage(escape.BOMB_DEACTIVATED);
 })
 
 escape.onUpdate(function () {
